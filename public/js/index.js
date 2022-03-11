29ea6b068c4346a9a3bc14cc1853e5f34ed9dc30
Aaron Mark <you@example.com>
2022-03-10 21:51:02 -0500

    New JS folder with containing main javascript components

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,4 @@
-const Utils = {
-	addTermToForm: cardNumber => {
-		if (typeof cardNumber != 'number') {
-			alert('something went wrong! error code: 01');
-			return;
-		}
-
-		let newCard = document.createElement('div');
-		newCard.className = 'form-item';
-		/* !! make sure this is safe !! */
-		newCard.innerHTML = `<label class="form-item-label">Card ${cardNumber}</label><br>
-		<input class="form-item-input" maxlength="110" type="text" placeholder="Enter term.." />
-		<input class="form-item-input" maxlength="110" type="text" placeholder="Enter definition.." />
-		<button class="form-delete-term">delete</button><br />`;
-
-		return newCard;
-	},
-};
-
+import Utils from './utils.js';
 class User {
 	constructor() {
 		this.selectors = {
